8ee5fd90f36c6220cdd080a8d2046e9daf45a2dc
asaf schers <schers@riskified.com>
2016-11-15 13:13:29 +0200

    random forester

--- lib/random_forester.rb
+++ lib/random_forester.rb
@@ -1,5 +1,32 @@
-require "random_forester/version"
+require 'random_forester/version'
+require 'nokogiri'
+require 'random_forest'
+
+RANDOM_FOREST_MODEL = 'randomForest_Model'
+MODEL_NOT_SUPPORTED_ERROR = 'model not supported'
 
 module RandomForester
-  # Your code goes here...
+  def self.get_model(pmml_file_name)
+    xml = get_xml(pmml_file_name)
+    new_model(xml)
+  end
+
+  def self.new_model(xml)
+    case get_model_type(xml)
+      when RANDOM_FOREST_MODEL
+        RandomForest.new(xml)
+      else
+        raise MODEL_NOT_SUPPORTED_ERROR
+      end
+  end
+
+  def self.get_xml(pmml_file_name)
+    pmml_string = File.open(pmml_file_name, 'rb').read
+    Nokogiri::XML(pmml_string)
+  end
+
+  def self.get_model_type(xml)
+    xml.remove_namespaces!
+    xml.xpath("PMML/MiningModel/@modelName").to_s
+  end
 end
