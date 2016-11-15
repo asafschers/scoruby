3411d2d6756517f587c7bde358f4472a5875c832
asaf schers <schers@riskified.com>
2016-11-15 13:13:29 +0200

    random forester

--- spec/random_forester_spec.rb
+++ spec/random_forester_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe RandomForester do
-  it 'has a version number' do
-    expect(RandomForester::VERSION).not_to be nil
+
+  RF_FILE = 'rf_file'
+  NON_RF_FILE = 'non_rf_file'
+
+  let(:rf_file) { 'spec/fixtures/rf_file.pmml' }
+  let(:non_rf_file) { 'spec/fixtures/non_rf_file.pmml' }
+
+  it 'raises when type not random forest' do
+    expect {
+      RandomForester.get_model(non_rf_file)
+    }.to raise_error(MODEL_NOT_SUPPORTED_ERROR)
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'inits random forest when type random forest' do
+    model = RandomForester.get_model(rf_file)
+    expect(model).to be_an_instance_of(RandomForest)
   end
 end
