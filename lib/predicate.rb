4550a69f0fd3ac8dbb9ae73d8d335fe3d306b4a7
asaf schers <schers@riskified.com>
2016-11-16 14:07:29 +0200

    create predicate model

---
+++ lib/predicate.rb
@@ -0,0 +1,19 @@
+class Predicate
+
+  def initialize(pred_xml)
+    @pred_xml = pred_xml
+  end
+
+  def to_s
+    @pred_xml.xpath('*')[0].to_s
+  end
+
+  def decision
+    @pred_xml.xpath('@score').to_s
+  end
+
+  def true?(features)
+    # TODO: implement
+    true
+  end
+end
