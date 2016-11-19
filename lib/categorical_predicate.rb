a9c7ee568d45e50c36f34cde4bc063206bf94f82
asaf schers <schers@riskified.com>
2016-11-19 02:04:02 +0200

    format data to predicate subclasses

--- lib/categorical_predicate.rb
+++ lib/categorical_predicate.rb
@@ -1,12 +1,13 @@
-
 class CategoricalPredicate
 
   def initialize(pred_xml)
-
+    @Array = pred_xml.xpath('Array')
+    @operator = pred_xml.xpath('@booleanOperator')
   end
 
   def true?(features)
-
+    puts @Array
+    puts @operator
   end
 
 end
--- lib/numerical_predicate.rb
+++ lib/numerical_predicate.rb
@@ -1,11 +1,12 @@
-
 class NumericalPredicate
 
   def initialize(pred_xml)
-
+    @value = pred_xml.xpath('@value')
+    @operator = pred_xml.xpath('@operator')
   end
 
   def true?(features)
-
+    puts @value
+    puts @operator
   end
 end
