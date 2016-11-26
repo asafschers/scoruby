cc1a49b70c710cbbfaa1570d5afeffa966700453
asaf schers <schers@riskified.com>
2016-11-26 20:23:18 +0200

    numerical predicate spec + validations

--- lib/numerical_predicate.rb
+++ lib/numerical_predicate.rb
@@ -11,7 +11,22 @@ class NumericalPredicate
   end
 
   def true?(features)
+    return if missing_feature?(features)
+    return if nil_feature?(features)
     return @value > features[@field] if @operator == GREATER_THAN
     @value < features[@field] if @operator == LESS_OR_EQUAL
   end
+
+  def missing_feature?(features)
+    return false if features.has_key? @field
+    RandomForester.logger.error "Missing feature #{@field}"
+    true
+  end
+
+  def nil_feature?(features)
+    return false unless features[@field].nil?
+    RandomForester.logger.error "Feature #{@field} value is nil"
+    true
+  end
+
 end
