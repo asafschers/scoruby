44d607759a317609832643d792dc650380f2c0bf
asaf schers <schers@riskified.com>
2016-11-30 18:27:20 +0200

    forest logic and spec

--- lib/random_forest.rb
+++ lib/random_forest.rb
@@ -5,15 +5,19 @@ class RandomForest
   def initialize(xml)
     xml_trees = xml.xpath("PMML/MiningModel/Segmentation/Segment/TreeModel/Node")
     @decision_trees = xml_trees.collect{ |xml_tree|
-      puts '1'
       DecisionTree.new(xml_tree)
     }
   end
 
-  def predict(features)
-    @decision_trees.count { |decision_tree|
-      decision_tree.decide(features) == DecisionTree::SHOULD_APPROVE
+  def decisions_count(fearures)
+    decisions = @decision_trees.collect { |decision_tree|
+      decision_tree.decide(fearures)
     }
+    decisions.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
+  end
+
+  def predict(features)
+    decisions_count(features).max_by {|_, v|  v }[0]
   end
 
 end
--- spec/random_forest_spec.rb
+++ spec/random_forest_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe RandomForest do
 
+  SHOULD_APPROVE = 'should_approve'
+  SHOULD_DECLINE = 'should_decline'
+
   before(:all) do
     rf_file = 'spec/fixtures/rf_file.pmml'
     xml = RandomForester.get_xml(rf_file)
@@ -10,13 +13,19 @@ describe RandomForest do
   end
 
   it 'predicts approve' do
-    (1..65).each { |i| @features[:"f#{i}"] = 3}
-    puts @random_forest.predict(@features)
+    (1..65).each { |i| @features[:"f#{i}"] = 0}
+    expect(@random_forest.predict(@features)).to eq SHOULD_APPROVE
+    decisions_count = @random_forest.decisions_count(@features)
+    expect(decisions_count[SHOULD_APPROVE]).to eq 160
+    expect(decisions_count[SHOULD_DECLINE]).to eq 140
   end
 
   it 'predicts decline' do
-    (1..65).each { |i| @features[:"f#{i}"] = 100}
-    puts @random_forest.predict(@features)
+    (1..65).each { |i| @features[:"f#{i}"] = 3000}
+    expect(@random_forest.predict(@features)).to eq SHOULD_DECLINE
+    decisions_count = @random_forest.decisions_count(@features)
+    expect(decisions_count[SHOULD_APPROVE]).to eq 142
+    expect(decisions_count[SHOULD_DECLINE]).to eq 158
   end
 
 end
