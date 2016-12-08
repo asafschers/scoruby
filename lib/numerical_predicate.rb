4fefae973d2ca80f6c6649d86399860ea4ac6812
asaf schers <schers@riskified.com>
2016-12-08 18:57:22 +0200

    fix numerical predicate bug

--- lib/numerical_predicate.rb
+++ lib/numerical_predicate.rb
@@ -7,12 +7,13 @@ class NumericalPredicate
 
   def initialize(pred_xml)
     @field = pred_xml.xpath('@field').to_s.to_sym
-    @value = pred_xml.xpath('@value').to_s.to_i
+    @value = Float(pred_xml.xpath('@value').to_s)
     @operator = pred_xml.xpath('@operator').to_s
   end
 
   def true?(features)
-    return @value > features[@field] if @operator == GREATER_THAN
-    @value < features[@field] if @operator == LESS_OR_EQUAL
+    curr_value = Float(features[@field])
+    return curr_value > @value if @operator == GREATER_THAN
+    curr_value < @value if @operator == LESS_OR_EQUAL
   end
 end
