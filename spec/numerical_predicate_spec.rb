962d0daeaa9823f6229afc8b08b1b0240829b6d8
asaf schers <schers@riskified.com>
2016-12-08 18:57:22 +0200

    fix numerical predicate bug

--- spec/numerical_predicate_spec.rb
+++ spec/numerical_predicate_spec.rb
@@ -8,11 +8,11 @@ describe NumericalPredicate do
   let (:numerical_predicate) { NumericalPredicate.new(relevant_pred_xml) }
 
   it 'returns true' do
-    expect(numerical_predicate.true?(f33: 19)).to eq true
+    expect(numerical_predicate.true?(f33: 18)).to eq true
   end
 
   it 'returns false' do
-    expect(numerical_predicate.true?(f33: 18)).to eq false
+    expect(numerical_predicate.true?(f33: 19)).to eq false
   end
 
 end
