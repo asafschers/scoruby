6ca8f72895a578a9a66a897a056606b9948038e2
asaf schers <schers@riskified.com>
2017-02-01 15:03:10 +0200

    add tiny spec

--- spec/random_forester_spec.rb
+++ spec/random_forester_spec.rb
@@ -4,11 +4,15 @@ describe RandomForester do
 
   let(:rf_file) { 'spec/fixtures/rf_file.pmml' }
   let(:non_rf_file) { 'spec/fixtures/non_rf_file.pmml' }
+  let(:decision_tree_file) { 'spec/fixtures/decision_tree.pmml' }
 
-  it 'raises when type not random forest' do
+  it 'raises when type not known' do
     expect {
       RandomForester.get_model(non_rf_file)
     }.to raise_error(MODEL_NOT_SUPPORTED_ERROR)
   end
 
+  it 'initializes random forest' do
+    expect(RandomForester.get_model(rf_file)).to be_a(RandomForest)
+  end
 end
