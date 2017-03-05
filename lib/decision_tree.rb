f1d30d4d4a691fd2a13cab3f682a72250a69d30c
asaf schers <schers@riskified.com>
2017-03-05 23:58:37 +0200

    Remove rubytree

--- lib/decision_tree.rb
+++ lib/decision_tree.rb
@@ -1,54 +1,36 @@
 require 'predicate'
-require 'rubytree'
 
 class DecisionTree
-  ROOT = 'root'
-  LEFT = 'left'
-  RIGHT = 'right'
 
   attr_reader :root
 
   def initialize(tree_xml)
     @id = tree_xml.attribute('id')
-    @root = Tree::TreeNode.new(ROOT)
-    set_node(tree_xml.xpath('TreeModel/Node'), @root)
+    @root = Predicate.new(tree_xml.xpath('TreeModel/Node'))
   end
 
   def decide(features)
     curr = @root
-    while curr.content.decision == ''
+    while curr.decision == ''
       prev = curr
       curr = step(curr, features)
       return if didnt_step?(curr, prev)
     end
 
-    curr.content.decision
+    curr.decision
   end
 
   private
 
-  def set_node(tree_xml, root)
-    children = tree_xml.children
-    root.content = Predicate.new(children[0], tree_xml.attribute('score').to_s)
-    return if children.count == 1
-
-    root << Tree::TreeNode.new(LEFT)
-    root << Tree::TreeNode.new(RIGHT)
-
-    set_node(children[1], root[LEFT]) if children[1]
-    set_node(children[2], root[RIGHT]) if children[2]
-  end
-
   def step(curr, features)
-    curr = curr[LEFT] if curr[LEFT] && curr[LEFT].content.true?(features)
-    curr = curr[RIGHT] if curr[RIGHT] && curr[RIGHT].content.true?(features)
+    curr = curr.left if curr.left && curr.left.true?(features)
+    curr = curr.right if curr.right && curr.right.true?(features)
     curr
   end
 
   def didnt_step?(curr, prev)
-    return false if (prev.content != curr.content)
-    RandomForester.logger.error "Null tree: #{@id}, bad feature: #{curr[LEFT].content.field }"
+    return false if (prev.pred != curr.pred)
+    RandomForester.logger.error "Null tree: #{@id}, bad feature: #{curr.left.pred.field }"
     true
   end
-
 end
