7fb11ab352a2d564b66d6fec8ab5215ba06e41dc
asaf schers <schers@riskified.com>
2017-04-03 01:45:25 +0300

    identify and init gbm

--- spec/random_forester_spec.rb
+++ spec/random_forester_spec.rb
@@ -5,6 +5,7 @@ describe RandomForester do
   let(:rf_file) { 'spec/fixtures/rf_file.pmml' }
   let(:non_rf_file) { 'spec/fixtures/non_rf_file.pmml' }
   let(:decision_tree_file) { 'spec/fixtures/decision_tree.pmml' }
+  let(:gbm_file) { 'spec/fixtures/gbm_file.pmml' }
 
   it 'raises when type not known' do
     expect {
@@ -14,5 +15,9 @@ describe RandomForester do
 
   it 'initializes random forest' do
     expect(RandomForester.get_model(rf_file)).to be_a(RandomForest)
+  end
+
+  it 'initializes gbm ' do
+    expect(RandomForester.get_model(gbm_file)).to be_a(Gbm)
   end
 end
