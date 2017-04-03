159c62eb034fe0d00b21fae9d885246b87b44b3a
asaf schers <schers@riskified.com>
2017-04-03 23:27:59 +0300

    better gbm specs

--- spec/gbm_spec.rb
+++ spec/gbm_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 
 describe Gbm do
 
+  let(:gbm_file) { 'spec/fixtures/gbm_sample.pmml'}
+  let(:xml) { RandomForester.xml_from_file_path(gbm_file) }
+  let(:gbm) { Gbm.new(xml) }
 
-
-  xit 'loads correct number of trees' do
-    gbm_file = 'spec/fixtures/gbm_sample.pmml'
-    xml = RandomForester.xml_from_file_path(gbm_file)
-    @gbm = Gbm.new(xml)
-    expect(@gbm.tree_count).to eq 30
+  it 'loads correct number of trees' do
+    expect(gbm.tree_count).to eq 30
   end
 
 end
