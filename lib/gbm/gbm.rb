0b9986421d0ef0959a11504eb30100009c9c1f94
asaf schers <schers@riskified.com>
2017-04-03 23:24:47 +0300

    add gbm class

--- lib/gbm/gbm.rb
+++ lib/gbm/gbm.rb
@@ -1,11 +1,20 @@
+require 'gbm_decision_tree'
+
 class Gbm
+  GBM_FOREST_XPATH = '//Segmentation[@multipleModelMethod="sum"]/Segment'
 
   def initialize(xml)
-      # TODO: spec + init forest
-      # TODO: spec + score forest
+
+    @gbm_forest = RandomForest.new(xml,GbmDecisionTree,GBM_FOREST_XPATH)
+
+
+  end
+
+  def tree_count
+    @gbm_forest.tree_count
   end
 
   def score(features)
-
+    # TODO: spec + code
   end
 end
