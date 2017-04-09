362deb946bf01c8297974468365b6e047fb071fe
asaf schers <schers@riskified.com>
2017-04-09 15:24:30 +0300

    add gbm node and logic

---
+++ lib/gbm/gbm_node.rb
@@ -0,0 +1,21 @@
+class GbmNode
+
+  attr_reader :score, :missing, :left, :right, :pred
+
+  def initialize(xml)
+    children = xml.children
+    @pred = Predicate.new(children[0])
+
+    @score = xml.attribute('score').to_s
+
+    return if children.count == 1
+    @missing = GbmNode.new(children[1]) if children[1]
+    @left = GbmNode.new(children[2]) if children[2]
+    @right = GbmNode.new(children[3]) if children[3]
+  end
+
+  def true?(features)
+    @pred.nil? || @pred.true?(features)
+  end
+
+end
