f42f2550c4511723228dbcc8014e393fd4a015f2
asaf schers <schers@riskified.com>
2017-04-09 15:24:43 +0300

    init logic to gbm decision tree

--- lib/gbm/gbm_decision_tree.rb
+++ lib/gbm/gbm_decision_tree.rb
@@ -1,5 +1,11 @@
-class GbmDecisionTree
-  def initialize(tree_xml)
+require 'gbm_node'
 
+class GbmDecisionTree
+  attr_reader :root
+
+  def initialize(tree_xml)
+    @id = tree_xml.attribute('id')
+    @root = GbmNode.new(tree_xml.xpath('TreeModel/Node'))
   end
+  
 end
