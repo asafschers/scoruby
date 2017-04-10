1e213f8c22305a43999b906deca7188000f34b68
asaf schers <schers@riskified.com>
2017-04-10 11:04:18 +0300

    node spec to predicate spec

--- spec/predicate_spec.rb
+++ spec/predicate_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe Node do
+describe Predicate do
 
   let (:pred_string) { '<SimplePredicate field="f33" operator="lessOrEqual" value="18.8513846048894"/>' }
   let (:pred_xml) { Nokogiri::XML(pred_string); }
-  let (:predicate) { Node.new(pred_xml) }
+  let (:predicate) { Predicate.new(pred_xml.children.first) }
 
   it 'logs missing feature' do
     expect(RandomForester.logger).to receive(:error).with('Missing feature f33')
