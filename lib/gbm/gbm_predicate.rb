272ffe605478e62813ca42ad7647b144bce5b05c
asaf schers <schers@riskified.com>
2017-04-10 13:40:27 +0300

    add GBM predicate w/o nil/missing checks

---
+++ lib/gbm/gbm_predicate.rb
@@ -0,0 +1,18 @@
+require 'simple_predicate'
+require 'simple_set_predicate'
+
+class GbmPredicate
+
+  def initialize(pred_xml)
+    @pred = SimplePredicate.new(pred_xml) if pred_xml.name == 'SimplePredicate'
+    @pred = SimpleSetPredicate.new(pred_xml) if pred_xml.name == 'SimpleSetPredicate'
+  end
+
+  def field
+    @pred.field
+  end
+
+  def true?(features)
+    @pred.true?(features)
+  end
+end
