b9da116f1152b6a3986e47dc9e9c889ff1baeea5
asaf schers <schers@riskified.com>
2017-04-10 12:44:00 +0300

    spec equal, isMissing operators for simple pred

--- spec/simple_predicate_spec.rb
+++ spec/simple_predicate_spec.rb
@@ -2,16 +2,54 @@ require 'spec_helper'
 
 describe SimplePredicate do
 
-  let (:pred_string) { '<SimplePredicate field="f33" operator="lessOrEqual" value="18.8513846048894"/>' }
-  let (:pred_xml) { Nokogiri::XML(pred_string); }
-  let (:numerical_predicate) { SimplePredicate.new(pred_xml.children.first) }
+  context 'less or equal predicate' do
+    let (:pred_string) { '<SimplePredicate field="f33" operator="lessOrEqual" value="18.8513846048894"/>' }
+    let (:pred_xml) { Nokogiri::XML(pred_string); }
+    let (:predicate) { SimplePredicate.new(pred_xml.children.first) }
 
-  it 'returns true' do
-    expect(numerical_predicate.true?(f33: 18)).to eq true
+    it 'returns true' do
+      expect(predicate.true?(f33: 18)).to eq true
+    end
+
+    it 'returns false' do
+      expect(predicate.true?(f33: 19)).to eq false
+    end
   end
 
-  it 'returns false' do
-    expect(numerical_predicate.true?(f33: 19)).to eq false
+  context 'is missing predicate' do
+    let (:pred_string) { '<SimplePredicate field="f33" operator="isMissing"/>' }
+    let (:pred_xml) { Nokogiri::XML(pred_string); }
+    let (:predicate) { SimplePredicate.new(pred_xml.children.first) }
+
+    it 'returns true' do
+      expect(predicate.true?(f33: nil)).to eq true
+    end
+
+    it 'returns false' do
+      expect(predicate.true?({})).to eq true
+    end
+
+    it 'returns false' do
+      expect(predicate.true?(f33: '6')).to eq false
+    end
+  end
+
+  context 'equals predicate' do
+    let (:pred_string) { '<SimplePredicate field="f33" operator="equal" value="f2v3"/>' }
+    let (:pred_xml) { Nokogiri::XML(pred_string); }
+    let (:predicate) { SimplePredicate.new(pred_xml.children.first) }
+
+    it 'returns true' do
+      expect(predicate.true?(f33: 'f2v3')).to eq true
+    end
+
+    it 'returns true' do
+      expect(predicate.true?(f33: nil)).to eq false
+    end
+
+    it 'returns false' do
+      expect(predicate.true?({})).to eq false
+    end
   end
 
 end
