77a64560350b3c0d916336b5acbd428b4ce9855e
asaf schers <schers@riskified.com>
2017-04-14 02:21:17 +0300

    gbm init trees

--- lib/gbm/gbm.rb
+++ lib/gbm/gbm.rb
@@ -4,15 +4,17 @@ class Gbm
   GBM_FOREST_XPATH = '//Segmentation[@multipleModelMethod="sum"]/Segment'
 
   def initialize(xml)
-    @xml_trees = xml.xpath(GBM_FOREST_XPATH)
+    xml_trees = xml.xpath(GBM_FOREST_XPATH)
+    @decision_trees = xml_trees.collect{ |xml_tree|
+      GbmDecisionTree.new(xml_tree)
+    }
   end
 
   def tree_count
-    @xml_trees.count
+    @decision_trees.count
   end
 
   def score(features)
-
+    
   end
-
 end
