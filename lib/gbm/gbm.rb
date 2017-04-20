477f0e9a997202883210957110cad6b62c2281ae
asaf schers <schers@riskified.com>
2017-04-20 22:31:46 +0300

    add const to gbm score

--- lib/gbm/gbm.rb
+++ lib/gbm/gbm.rb
@@ -2,12 +2,13 @@ require 'gbm_decision_tree'
 
 class Gbm
   GBM_FOREST_XPATH = '//Segmentation[@multipleModelMethod="sum"]/Segment'
+  CONST_XPATH      = '//Constant[@dataType="double"]'
 
   def initialize(xml)
-    xml_trees = xml.xpath(GBM_FOREST_XPATH)
-    @decision_trees = xml_trees.collect{ |xml_tree|
+    @decision_trees = xml.xpath(GBM_FOREST_XPATH).collect{ |xml_tree|
       GbmDecisionTree.new(xml_tree)
     }
+    @const = Float(xml.xpath(CONST_XPATH).children[0].content)
   end
 
   def tree_count
@@ -15,6 +16,9 @@ class Gbm
   end
 
   def score(features)
-    @decision_trees.map { |dt| dt.decide(features) }.reduce(:+)
+    @decision_trees.map { |dt| dt.decide(features) }.reduce(:+) + @const
+
+
+    # TODO: add const and run exp(a) / (1+exp(a))
   end
 end
