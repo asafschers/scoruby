560fd48e7f12c5d4946a3cef4d5f77f01642257b
asaf schers <schers@riskified.com>
2017-07-09 00:40:00 +0300

    compound predicate spec and/or

--- spec/predicates/compound_predicate_spec.rb
+++ spec/predicates/compound_predicate_spec.rb
@@ -1,8 +1,42 @@
-require 'rspec'
+require 'spec_helper'
 
 describe CompoundPredicate do
 
-  it 'should do something' do
-    #true.should == false
+  context 'evaluates and' do
+    let (:pred_string) { p <<-XML
+          <CompoundPredicate booleanOperator="and"><True /><SimplePredicate field="f" operator="lessOrEqual" value="16.0918513223731" /></CompoundPredicate>
+    XML
+    }
+    let (:pred_xml) { Nokogiri::XML(pred_string); }
+    let (:predicate) { CompoundPredicate.new(pred_xml.children.first) }
+
+    it 'returns true' do
+      expect(predicate.true?(f: 16)).to eq true
+    end
+
+    it 'returns false' do
+      expect(predicate.true?(f: 17)).to eq false
+    end
+  end
+
+  context 'evaluates or' do
+    let (:pred_string) { p <<-XML
+          <CompoundPredicate booleanOperator="or"><False /><SimplePredicate field="f" operator="lessOrEqual" value="16.0918513223731" /></CompoundPredicate>
+    XML
+    }
+    let (:pred_xml) { Nokogiri::XML(pred_string); }
+    let (:predicate) { CompoundPredicate.new(pred_xml.children.first) }
+
+    it 'returns true' do
+      expect(predicate.true?(f: 16)).to eq true
+    end
+
+    it 'returns false' do
+      expect(predicate.true?(f: 17)).to eq false
+    end
+  end
+
+  context 'evaluates surrogate' do
+
   end
 end
