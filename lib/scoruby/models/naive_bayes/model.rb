3beb56b72b680a99615e9b451ff8cf208e929762
asaf schers <schers@riskified.com>
2017-09-15 12:05:20 +0300

    NavieBayes - extract some methods

--- lib/scoruby/models/naive_bayes/model.rb
+++ lib/scoruby/models/naive_bayes/model.rb
@@ -7,44 +7,51 @@ module Scoruby
       class Model
         extend Forwardable
         attr_reader :model_data
-        def_delegators :@model_data, :labels, :data, :threshold
+        def_delegators :@model_data, :data, :threshold, :labels
 
         def initialize(xml)
           @model_data = ModelData.new(xml)
-
         end
 
         def lvalues(features)
+          calc_label_feature_values(features)
+          calc_label_values
+        end
+
+        def score(features, label)
+          lvalues(features)[label] / lvalues(features).values.reduce(:+)
+        end
+
+        private
+
+        def calc_label_values
+          label_values = {}
+          labels.each do |label, label_data|
+            label_data.each do |key, value|
+              label_data[key] = threshold if value.round(5).zero?
+            end
+            label_values[label] = label_data.values.reduce(:*)
+          end
+          label_values
+        end
+
+        def calc_label_feature_values(features)
           labels.each do |label, _|
             features.each do |feature_name, feature_value|
-
               if data[feature_name][feature_value]
-                value_count = data[feature_name][feature_value][label].to_f
-                overall_count = data[feature_name].sum { |_, value| value[label].to_f }
-
-                labels[label][feature_name] = value_count / overall_count
+                labels[label][feature_name] = calc_category(feature_name, feature_value, label)
               elsif data[feature_name][label]
                 labels[label][feature_name] = calc_numerical(data[feature_name][label], feature_value)
               end
             end
           end
-
-          lvalues = {}
-          labels.each do |label, label_data|
-            label_data.each do |key, value|
-              label_data[key] = threshold if value.round(5).zero?
-            end
-            lvalues[label] = label_data.values.reduce(:*)
-          end
-          lvalues
         end
 
-        def score(features, label)
-          lvalues = lvalues(features)
-          lvalues[label] / lvalues.values.reduce(:+)
+        def calc_category(feature_name, feature_value, label)
+          value_count = data[feature_name][feature_value][label].to_f
+          overall_count = data[feature_name].sum { |_, value| value[label].to_f }
+          value_count / overall_count
         end
-
-        private
 
         def calc_numerical(label_data, feature_value)
           variance = label_data[:variance].to_f
