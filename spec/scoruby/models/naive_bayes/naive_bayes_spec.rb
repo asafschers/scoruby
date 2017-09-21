f2f82025dde3fc78e3cba0a2b74b7ebb774baf6e
asaf schers <schers@riskified.com>
2017-09-21 16:43:44 +0300

    NaiveBayes - finalize specs

--- spec/scoruby/models/naive_bayes/naive_bayes_spec.rb
+++ spec/scoruby/models/naive_bayes/naive_bayes_spec.rb
@@ -15,11 +15,24 @@ describe Scoruby::Models::NaiveBayes::Model do
   }
   end
 
-  it 'returns lvalues' do
-    puts naive_bayes.lvalues(features)
+  let(:l0) { 8723 * 0.001 * 4273/8598 * 225/8561 * 830/8008 }
+  let(:l1) { 2557 * (Math.exp(-(24 - 24.936)**2 / (2 * 0.516) ) / Math.sqrt(Math::PI*2 * 0.516)) * 1321/2533 * 10/2436 * 182/2266 }
+  let(:l2) { 1530 * (Math.exp(-(24 - 24.588)**2 / (2 * 0.635) ) / Math.sqrt(Math::PI*2 * 0.635)) * 780/1522 * 9/1496 * 51/1191 }
+  let(:l3) { 709 * (Math.exp(-(24 - 24.428)**2 / (2 * 0.379) ) / Math.sqrt(Math::PI*2 * 0.379)) * 405/697 * 0.001 * 26/699 }
+  let(:l4) { 100 * (Math.exp(-(24 - 24.770)**2 / (2 * 0.314) ) / Math.sqrt(Math::PI*2 * 0.314)) * 42/90 * 10/98 * 6/87 }
+
+  let(:lvalues) { naive_bayes.lvalues(features) }
+  let(:l2_probability) { naive_bayes.score(features, '1000') }
+
+  it 'calculates lvalues by http://dmg.org/pmml/v4-2-1/NaiveBayes.html' do
+    expect(lvalues['100']).to be_within(0.0001).of l0
+    expect(lvalues['500']).to be_within(0.0001).of l1
+    expect(lvalues['1000']).to be_within(0.0001).of l2
+    expect(lvalues['5000']).to be_within(0.0001).of l3
+    expect(lvalues['10000']).to be_within(0.0001).of l4
   end
 
-  it 'scores' do
-    puts naive_bayes.score(features, '1000')
+  it 'scores by http://dmg.org/pmml/v4-2-1/NaiveBayes.html' do
+    expect(l2_probability).to be_within(0.0001).of (l2 / (l0 + l1 + l2 + l3 + l4))
   end
 end
