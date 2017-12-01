c71cacaca177e8cbe38cf002a443c8161d36b45d
asaf schers <schers@riskified.com>
2017-12-02 01:31:33 +0200

    PredicateFactory - styling

--- lib/scoruby/predicate_factory.rb
+++ lib/scoruby/predicate_factory.rb
@@ -8,14 +8,11 @@ require 'scoruby/predicates/false_predicate'
 
 module Scoruby
   class PredicateFactory
-
     def self.for(pred_xml)
-      return Predicates::SimplePredicate.new(pred_xml) if pred_xml.name == 'SimplePredicate'
-      return Predicates::SimpleSetPredicate.new(pred_xml) if pred_xml.name == 'SimpleSetPredicate'
-      return Predicates::CompoundPredicate.new(pred_xml) if pred_xml.name == 'CompoundPredicate'
       return Predicates::TruePredicate.new if pred_xml.name == 'True'
       return Predicates::FalsePredicate.new if pred_xml.name == 'False'
+      predicate = Object.const_get("Scoruby::Predicates::#{pred_xml.name}")
+      predicate.new pred_xml
     end
   end
 end
-
