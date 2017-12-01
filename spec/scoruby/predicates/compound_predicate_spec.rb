af0812250db6f2fe9afbcbfac1f62d7fb5dfff77
asaf schers <schers@riskified.com>
2017-12-01 16:18:29 +0200

    CompoundPredicateSpec - styling

--- spec/scoruby/predicates/compound_predicate_spec.rb
+++ spec/scoruby/predicates/compound_predicate_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 
 describe Scoruby::Predicates::CompoundPredicate do
-
   context 'evaluates and' do
-    let (:pred_string) {  <<-XML
-          <CompoundPredicate booleanOperator="and"><True /><SimplePredicate field="f" operator="lessOrEqual" value="16.0918513223731" /></CompoundPredicate>
-    XML
-    }
-    let (:pred_xml) { Nokogiri::XML(pred_string); }
-    let (:predicate) { described_class.new(pred_xml.children.first) }
+    let(:pred_string) do
+      <<-XML
+        <CompoundPredicate booleanOperator="and">
+          <True />
+          <SimplePredicate field="f" operator="lessOrEqual" value="16.0918513223731" />
+        </CompoundPredicate>
+      XML
+    end
+
+    let(:pred_xml) { Nokogiri::XML(pred_string); }
+    let(:predicate) { described_class.new(pred_xml.children.first) }
 
     it 'returns true' do
       expect(predicate.true?(f: 16)).to eq true
@@ -20,12 +24,14 @@ describe Scoruby::Predicates::CompoundPredicate do
   end
 
   context 'evaluates or' do
-    let (:pred_string) {  <<-XML
-          <CompoundPredicate booleanOperator="or"><False /><SimplePredicate field="f" operator="lessOrEqual" value="16.0918513223731" /></CompoundPredicate>
-    XML
-    }
-    let (:pred_xml) { Nokogiri::XML(pred_string); }
-    let (:predicate) { described_class.new(pred_xml.children.first) }
+    let(:pred_string) do
+      <<-XML
+        <CompoundPredicate booleanOperator="or"><False /><SimplePredicate field="f" operator="lessOrEqual" value="16.0918513223731" /></CompoundPredicate>
+      XML
+    end
+
+    let(:pred_xml) { Nokogiri::XML(pred_string); }
+    let(:predicate) { described_class.new(pred_xml.children.first) }
 
     it 'returns true' do
       expect(predicate.true?(f: 16)).to eq true
@@ -37,14 +43,14 @@ describe Scoruby::Predicates::CompoundPredicate do
   end
 
   context 'evaluates surrogate' do
-
     context 'simple predicate' do
-      let (:pred_string) {  <<-XML
+      let(:pred_string) do
+        <<-XML
           <CompoundPredicate booleanOperator="surrogate"><SimplePredicate field="f" operator="lessOrEqual" value="16.0918513223731" /><False /></CompoundPredicate>
-      XML
-      }
-      let (:pred_xml) { Nokogiri::XML(pred_string); }
-      let (:predicate) { described_class.new(pred_xml.children.first) }
+        XML
+      end
+      let(:pred_xml) { Nokogiri::XML(pred_string); }
+      let(:predicate) { described_class.new(pred_xml.children.first) }
 
       it 'missing' do
         expect(predicate.true?(g: 17)).to eq false
