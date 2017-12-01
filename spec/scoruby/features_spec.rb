989bd5871b273674d8df096a2e455a1f4bf4b6f5
asaf schers <schers@riskified.com>
2017-12-01 18:00:28 +0200

    FeaturesSpec - styling

--- spec/scoruby/features_spec.rb
+++ spec/scoruby/features_spec.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Scoruby::Features do
+  let(:features) { { f2: true, f3: false } }
 
-  let(:features) {{ f2: true, f3: false }}
-  
   it 'formats booleans' do
     expect(described_class.new(features).formatted).to eq(f2: 't', f3: 'f')
   end
