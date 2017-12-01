1d62dc59c0abc164d83f2e5de750161df08160d6
asaf schers <schers@riskified.com>
2017-12-01 16:32:13 +0200

    RandomForestSpec - styling

--- spec/scoruby/models/random_forest_spec.rb
+++ spec/scoruby/models/random_forest_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Scoruby::Models::RandomForest do
-
   let(:rf_file) { 'spec/fixtures/titanic_rf.pmml' }
   let(:xml) { Scoruby.xml_from_file_path(rf_file) }
   let(:random_forest) { described_class.new(xml) }
@@ -9,40 +8,41 @@ describe Scoruby::Models::RandomForest do
   let(:decisions_count) { random_forest.decisions_count(features) }
 
   context '0 features' do
-    let(:features) {
+    let(:features) do
       {
-          Sex: 'male',
-          Parch: 0,
-          Age: 30,
-          Fare: 9.6875,
-          Pclass: 2,
-          SibSp: 0,
-          Embarked: 'Q'
+        Sex: 'male',
+        Parch: 0,
+        Age: 30,
+        Fare: 9.6875,
+        Pclass: 2,
+        SibSp: 0,
+        Embarked: 'Q'
       }
-    }
+    end
     it 'predicts 0' do
       expect(prediction[:label]).to eq '0'
-      expect(prediction[:score]).to eq 13/15.to_f
+      expect(prediction[:score]).to eq 13 / 15.to_f
       expect(decisions_count['0']).to eq 13
       expect(decisions_count['1']).to eq 2
     end
   end
 
   context '1 features' do
-    let(:features) {
+    let(:features) do
       {
-          Sex: 'female',
-          Parch: 0,
-          Age: 38,
-          Fare: 71.2833,
-          Pclass: 2,
-          SibSp: 1,
-          Embarked: 'C'
+        Sex: 'female',
+        Parch: 0,
+        Age: 38,
+        Fare: 71.2833,
+        Pclass: 2,
+        SibSp: 1,
+        Embarked: 'C'
       }
-    }
+    end
+
     it 'predicts 0' do
       expect(prediction[:label]).to eq '1'
-      expect(prediction[:score]).to eq 14/15.to_f
+      expect(prediction[:score]).to eq 14 / 15.to_f
       expect(decisions_count['0']).to eq 1
       expect(decisions_count['1']).to eq 14
     end
