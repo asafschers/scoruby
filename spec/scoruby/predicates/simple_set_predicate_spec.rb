4944039616c98c70b11be98cee49b2278596cb57
asaf schers <schers@riskified.com>
2017-12-01 16:00:05 +0200

    SimpleSetPredicate spec - styling

--- spec/scoruby/predicates/simple_set_predicate_spec.rb
+++ spec/scoruby/predicates/simple_set_predicate_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
 describe Scoruby::Predicates::SimpleSetPredicate do
-
-  let (:pred_xml) { Scoruby.xml_from_string(pred_string) }
-  let (:relevant_pred_xml) {  pred_xml.children[0] }
-  let (:categorical_predicate) { described_class.new(relevant_pred_xml) }
+  let(:pred_xml) { Scoruby.xml_from_string(pred_string) }
+  let(:relevant_pred_xml) { pred_xml.children[0] }
+  let(:categorical_predicate) { described_class.new(relevant_pred_xml) }
 
   context 'quotes' do
-    let (:pred_string) { '<SimpleSetPredicate field="f36" booleanOperator="isIn">
-                          <Array n="6" type="string">&quot;Missing&quot;   &quot;No Match&quot;</Array>
-                          </SimpleSetPredicate>'
-    }
+    let(:pred_string) do
+      <<-XML
+        <SimpleSetPredicate field="f36" booleanOperator="isIn">
+          <Array n="6" type="string">&quot;Missing&quot;   &quot;No Match&quot;</Array>
+        </SimpleSetPredicate>
+      XML
+    end
 
     it 'returns true' do
       expect(categorical_predicate.true?(f36: 'No Match')).to eq true
@@ -20,13 +22,15 @@ describe Scoruby::Predicates::SimpleSetPredicate do
       expect(categorical_predicate.true?(f36: 'Match')).to eq false
     end
   end
-  
-  context 'no quotes' do
 
-    let (:pred_string) { '<SimpleSetPredicate field="f36" booleanOperator="isIn">
-                          <Array n="6" type="string">f2v1 f2v2 f2v3</Array>
-                          </SimpleSetPredicate>'
-    }
+  context 'no quotes' do
+    let(:pred_string) do
+      <<-XML
+        <SimpleSetPredicate field="f36" booleanOperator="isIn">
+          <Array n="6" type="string">f2v1 f2v2 f2v3</Array>
+        </SimpleSetPredicate>
+      XML
+    end
 
     it 'returns true' do
       expect(categorical_predicate.true?(f36: 'f2v2')).to eq true
