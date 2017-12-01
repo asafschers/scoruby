5c98e75bfbe41c03d87cb91b1cc207f5f59affc2
asaf schers <schers@riskified.com>
2017-12-01 16:59:22 +0200

    GbmSpec - styling

--- spec/scoruby/models/gbm_spec.rb
+++ spec/scoruby/models/gbm_spec.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Scoruby::Models::Gbm do
-
-  let(:gbm_file) { 'spec/fixtures/titanic_gbm.pmml'}
+  let(:gbm_file) { 'spec/fixtures/titanic_gbm.pmml' }
   let(:xml) { Scoruby.xml_from_file_path(gbm_file) }
   let(:gbm) { described_class.new(xml) }
 
@@ -12,29 +13,28 @@ describe Scoruby::Models::Gbm do
 
   def approve_features
     {
-        Sex: 'male',
-        Parch: 0,
-        Age: 30,
-        Fare: 9.6875,
-        Pclass: 2,
-        SibSp: 0,
-        Embarked: 'Q'
-
+      Sex: 'male',
+      Parch: 0,
+      Age: 30,
+      Fare: 9.6875,
+      Pclass: 2,
+      SibSp: 0,
+      Embarked: 'Q'
     }
   end
 
   def decline_features
     {
-        Sex: 'female',
-        Parch: 0,
-        Age: 38,
-        Fare: 71.2833,
-        Pclass: 2,
-        SibSp: 1,
-        Embarked: 'C'
+      Sex: 'female',
+      Parch: 0,
+      Age: 38,
+      Fare: 71.2833,
+      Pclass: 2,
+      SibSp: 1,
+      Embarked: 'C'
     }
   end
-  
+
   it 'predicts approve' do
     expect(gbm.score(approve_features)).to eq 0.3652639329522468
   end
@@ -42,5 +42,4 @@ describe Scoruby::Models::Gbm do
   it 'predicts decline' do
     expect(gbm.score(decline_features)).to eq 0.4178155014037758
   end
-
 end
