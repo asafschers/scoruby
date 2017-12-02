e44ec1118f878b73d2cf5016a82cad268fe7b897
asaf schers <schers@riskified.com>
2017-12-02 14:03:43 +0200

    RandomForest - styling

--- lib/scoruby/models/random_forest.rb
+++ lib/scoruby/models/random_forest.rb
@@ -7,25 +7,38 @@ module Scoruby
 
       def initialize(xml)
         xml_trees       = xml.xpath(RF_FOREST_XPATH)
-        @decision_trees = xml_trees.collect {|xml_tree|
+        @decision_trees = xml_trees.map do |xml_tree|
           DecisionTree.new(xml_tree)
+        end
+      end
+
+      def predict(features)
+        decisions_count = decisions_count(features)
+        decision = decisions_count.max_by { |_, v| v }
+        {
+          label: decision[0],
+          score: decision[1] / decisions_count.values.reduce(0, :+).to_f
         }
       end
 
       def decisions_count(features)
         formatted_features = Features.new(features).formatted
-        decisions          = @decision_trees.collect {|decision_tree|
-          decision_tree.decide(formatted_features).score
-        }
-        decisions.inject(Hash.new(0)) {|h, e| h[e] += 1; h}
+        decisions = traverse_trees(formatted_features)
+        aggregate_decisions(decisions)
       end
 
-      def predict(features)
-        decisions_count = decisions_count(features)
-        {
-            label: decisions_count.max_by {|_, v| v}[0],
-            score: decisions_count.max_by {|_, v| v}[1] / decisions_count.values.reduce(0, :+).to_f
-        }
+      private
+
+      def traverse_trees(formatted_features)
+        @decision_trees.map do |decision_tree|
+          decision_tree.decide(formatted_features).score
+        end
+      end
+
+      def aggregate_decisions(decisions)
+        decisions.each_with_object(Hash.new(0)) do |score, counts|
+          counts[score] += 1
+        end
       end
     end
   end
