63bde104312b9550572a04270c6a2fdef476ad63
asaf schers <schers@riskified.com>
2017-12-22 23:53:07 +0200

    RandomForest data - add continuous features method

--- lib/scoruby/models/random_forest/data.rb
+++ lib/scoruby/models/random_forest/data.rb
@@ -4,7 +4,8 @@ module Scoruby
   module Models
     module RandomForest
       RF_FOREST_XPATH = 'PMML/MiningModel/Segmentation/Segment'
-      
+      FEATURES_XPATH = 'PMML/DataDictionary/DataField'
+
       class Data
         def initialize(xml)
           @xml = xml
@@ -13,6 +14,18 @@ module Scoruby
         def decision_trees
           @xml.xpath(RF_FOREST_XPATH).map do |xml_tree|
             DecisionTree.new(xml_tree)
+          end
+        end
+
+        def continuous_features
+          continuous_features_nodes.map { |feature| feature.attr('name') }
+        end
+
+        private
+
+        def continuous_features_nodes
+          @xml.xpath(FEATURES_XPATH).select do |feature|
+            feature.attr('optype') == 'continuous'
           end
         end
       end
